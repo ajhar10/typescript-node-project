c732907695146bed8b9bc39da323900dd4e17d81
Your Name <you@example.com>
2023-11-23 20:09:55 +0600

    create student feature

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import { StudentRoutes } from './app/modules/student/student.route'
 const app: Application = express()
 
 app.use(cors())
 app.use(express.json())
-
+//routes
+app.use('/api/v1/students', StudentRoutes)
 app.get('/', (req: Request, res: Response) => {
   const a = 99
   res.send(a)
